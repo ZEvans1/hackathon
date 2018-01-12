75b1bbd3a6bd7e23c22e6baae3da2b8a3284b393
ZEvans1 <zte.zachary@gmail.com>
2018-01-12 10:09:50 -0800

    Create test for adding members to member list. Fails.

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -6,16 +6,20 @@ public class Team {
     private String name;
     private String description;
     private String member;
+    private String newMember;
     private ArrayList<String> memberList;
     private static ArrayList<Team> instances = new ArrayList<>();
 
 
 
-    public Team(String name, String description, String member) {
+    public Team(String name, String description, String member, ArrayList<String> memberList) {
         this.name = name;
         this.description = description;
         this.member = member;
-        //this.memberList.add(this.member);
+        this.memberList = memberList;
+        memberList.add(member);
+        instances.add(this);
+
     }
 
     public String getName() {
@@ -28,6 +32,14 @@ public class Team {
 
     public String getMember() {
         return member;
+    }
+
+    public String getNewMember() {
+        return newMember;
+    }
+
+    public ArrayList<String> getMemberList() {
+        return memberList;
     }
 
     public static ArrayList<Team> getAll() {
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -3,6 +3,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class TeamTest {
@@ -30,8 +32,14 @@ public class TeamTest {
         assertEquals("A team", testTeam.getDescription());
         assertEquals("Member1", testTeam.getMember());
     }
+
+    @Test
+    public void testTeam_addsNewMembers() {
+        Team testTeam = setUpNewTeam();
+        assertEquals(2, testTeam.getMemberList().size());
+    }
     //helper
     public Team setUpNewTeam() {
-        return new Team("Team1", "A team", "Member1");
+        return new Team("Team1", "A team", "Member1", new ArrayList<String>());
     }
 }
