37043541abf0e4890d22056071099fbb1942d4a7
ZEvans1 <zte.zachary@gmail.com>
2018-01-12 08:48:02 -0800

    Initial commit.

---
+++ src/test/java/TeamTest.java
@@ -0,0 +1,17 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TeamTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
